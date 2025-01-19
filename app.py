d583c1f3e52b211884baf98a6c35325dc6a4b011
ANIRUDH V KUMAR <118814650+ANIRUDHVKUMAR@users.noreply.github.com>
2025-01-19 09:54:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.secret_key = 'your_secret_key'
 # MySQL Configuration
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'your_mysql_password'
+app.config['MYSQL_PASSWORD'] = '************'
 app.config['MYSQL_DB'] = 'eventus_db'
 
 mysql = MySQL(app)
